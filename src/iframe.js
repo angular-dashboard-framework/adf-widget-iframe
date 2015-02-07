ab65148db17588abd302822232e341457de00c7e
Sebastian Sdorra <s.sdorra@gmail.com>
2015-02-07 15:13:27 +0100

    set title and description

--- src/iframe.js
+++ src/iframe.js
@@ -4,8 +4,8 @@ angular.module('adf.widget.iframe', ['adf.provider'])
   .config(function(dashboardProvider){
     dashboardProvider
       .widget('iframe', {
-        title: 'iframe Title',
-        description: 'Description of iframe',
+        title: 'iframe',
+        description: 'Embed an external page into the dashboard',
         templateUrl: '{widgetsPath}/iframe/src/view.html',
         controller: 'iframeController',
         controllerAs: 'iframe',
