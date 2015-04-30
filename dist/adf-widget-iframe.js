cea6d216d50a9fa7035acfe38ccb1046922d52de
Sebastian Sdorra <s.sdorra@gmail.com>
2015-04-30 20:20:39 +0200

    release v0.1.1

--- dist/adf-widget-iframe.js
+++ dist/adf-widget-iframe.js
@@ -1,7 +1,8 @@
-'use strict';
+(function(window, undefined) {'use strict';
+
 
 angular.module('adf.widget.iframe', ['adf.provider'])
-  .config(function(dashboardProvider){
+  .config(["dashboardProvider", function(dashboardProvider){
     dashboardProvider
       .widget('iframe', {
         title: 'iframe',
@@ -16,12 +17,12 @@ angular.module('adf.widget.iframe', ['adf.provider'])
           height: '420px'
         }
       });
-  })
-  .controller('iframeController', function($sce, config){
+  }])
+  .controller('iframeController', ["$sce", "config", function($sce, config){
     if (config.url){
       this.url = $sce.trustAsResourceUrl(config.url);
     }
-  });
+  }]);
 
 angular.module("adf.widget.iframe").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/iframe/src/edit.html","<form role=form><div class=form-group><label for=url>URL</label> <input type=url class=form-control id=url ng-model=config.url placeholder=http://www.example.com></div><div class=form-group><label for=url>Height</label> <input type=text class=form-control id=url ng-model=config.height></div></form>");
-$templateCache.put("{widgetsPath}/iframe/src/view.html","<div><div class=\"alert alert-info\" ng-if=!config.url>Please insert a url in the widget configuration</div><iframe ng-if=iframe.url class=adf-iframe style=\"height: {{config.height}}\" src={{iframe.url}}></iframe></div>");}]);
+$templateCache.put("{widgetsPath}/iframe/src/view.html","<div><div class=\"alert alert-info\" ng-if=!config.url>Please insert a url in the widget configuration</div><iframe ng-if=iframe.url class=adf-iframe style=\"height: {{config.height}}\" src={{iframe.url}}></iframe></div>");}]);})(window);
